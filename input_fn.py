e3324cfde620e39f4ad401f55418a97ea1adee16
Uday Kumar Gurugubelli <udaygurugubelli@gmail.com>
2019-01-07 01:17:14 +0530

    updated

--- input_fn.py
+++ input_fn.py
@@ -6,40 +6,67 @@ import itertools as it
 from operator import itemgetter
 import threading
 import time
+from siamese_model import SiameseNet, model_fn
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
 train_file= "./train_data/whaleDetection_train_data.tfrecords"
 test_file= "./test_data/whaleDetection_test_data.tfrecords"
-
-from one_shot import SiameseNet, model_fn
-
 tr_itr=tf.python_io.tf_record_iterator(train_file)
-tr_itr1=tf.python_io.tf_record_iterator(train_file)
 ts_itr=tf.python_io.tf_record_iterator(test_file)
-count=0
-for rc in tr_itr:
-    count = count+1
-print("num of records:",count)
-for rc in ts_itr:
-    count = count+1
-print("num of records:",count)
-def parse_fn(rcrd):
-    ex=tf.train.Example()
-    ex.ParseFromString(rcrd)
-    [lbl]=ex.features.feature['label'].bytes_list.value
-    lbl = tf.cast(lbl, tf.string)
-    [raw]=ex.features.feature['image_raw'].bytes_list.value
-    img=tf.cast(tf.io.decode_raw(raw, tf.int64), tf.uint8)
-    img=tf.cast(tf.reshape(img,(64,64,1)),tf.float32)
-#img=tf.image.per_image_standardization(img)
-    img=tf.reshape(img,[64,64])
+
+def parse_fn(record):
+    kys_to_fts = {"image_name":tf.FixedLenFeature((), tf.string),
+                    "label":tf.FixedLenFeature((), tf.string, default_value=""),
+                            "image_raw":tf.FixedLenFeature((), tf.string)}
+    parsed = tf.parse_single_example(record, kys_to_fts)
+
+    lbl=tf.cast(parsed["label"], tf.string)
+    img=tf.io.decode_raw(parsed["image_raw"], tf.uint8)
+    img=tf.cast(tf.reshape(img,(64,64,3)),tf.float32)
     return img, lbl
-def b_ip_fn():
-    f, l=tf.contrib.training.stratified_sample(tr_product_list, tf.convert_to_tensor(tr_label_list),
-            batch_size=20, enqueue_many=True, queue_capacity=100,
-            target_probs=tf.convert_to_tensor([0.5,0.5]),  threads_per_queue=2)
+
+def ip_fn():
+#   this repetition of code is required to cover the same images of same class    
+    ds1 = tf.data.TFRecordDataset(train_file)
+    ds1 = ds1.map(parse_fn)
+    ds1 = ds1.filter(lambda x,y:tf.equal(y,"new_whale"))
+    ds1 = ds1.repeat(100)
+    ds1 = ds1.shuffle(1000)
+    itr1 = ds1.make_one_shot_iterator()
+    img1,l1 = itr1.get_next()
+
+    ds2 = tf.data.TFRecordDataset(train_file)
+    ds2 = ds2.map(parse_fn)
+    ds2 = ds2.filter(lambda x,y:tf.equal(y,"new_whale"))
+    ds2 = ds2.repeat(100)
+    ds2 = ds2.shuffle(1000)
+    itr2 = ds2.make_one_shot_iterator()
+    img2,l2 = itr2.get_next()
+
+    y = tf.cast(tf.equal(l1,l2),tf.int32)
+   
+    im_set = (img1,img2)
+    f, l=tf.contrib.training.stratified_sample([im_set], y,
+                            batch_size=20, enqueue_many=False, queue_capacity=1000,
+                                            target_probs=tf.convert_to_tensor([0.5,0.5]),  threads_per_queue=4)
+
     return f, l
 
+def pred_ip_fn():
+#   this repetition of code is required to cover the same images of same class    
+    ds1 = tf.data.TFRecordDataset(test_file)
+    ds1 = ds1.map(parse_fn)
+    ts_itr = ds1.make_one_shot_iterator()
+    img1,l1 = ts_itr.get_next()
 
-est = tf.estimator.Estimator(model_fn=model_fn, model_dir="./oneshot_model_dir/")
-for _ in range(1):
-    est.train(input_fn = b_ip_fn, steps = 100000)
-    est.evaluate(input_fn=b_ip_fn,steps=100)
+    ds2 = tf.data.TFRecordDataset(train_file)
+    ds2 = ds2.map(parse_fn)
+    ds2 = ds2.filter(lambda x,y:tf.equal(y,"new_whale"))
+    tr_itr = ds2.make_one_shot_iterator()
+    img2,l2 = tr_itr.get_next()
+   
+    for img1, l1 in ts_itr:
+        for img2, l2 in tr_itr:
+            return (img1,img2), None
+     
